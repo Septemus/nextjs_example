d80420862ba847f6b89cf0f8ebe9e01e5f221325
joe <musketeerdt@gmail.com>
2025-04-18 00:40:58 +0800

    eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import "@/app/ui/global.scss"
+import '@/app/ui/global.scss';
 import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
