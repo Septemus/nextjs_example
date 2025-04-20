59cef50a22782f3e9d6cc28badc748276e19e463
joe <musketeerdt@gmail.com>
2025-04-20 22:28:01 +0800

    login and logout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig([
 			'no-unused-vars': 'warn',
 			'@typescript-eslint/no-unused-vars': 'warn',
 			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-explicit-any': 'warn',
 		},
 	},
 	eslintPluginPrettierRecommended,
