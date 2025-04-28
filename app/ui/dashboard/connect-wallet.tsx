4eb9fae011225c2595173157bd58bc563eb36042
joe <musketeerdt@gmail.com>
2025-04-28 21:42:20 +0800

    connect wallet

--- app/ui/dashboard/connect-wallet.tsx
+++ app/ui/dashboard/connect-wallet.tsx
@@ -1,26 +1,22 @@
 'use client';
-import { Button } from 'antd';
 import { useAccount, useConnect } from 'wagmi';
-
+import { ConnectButton } from '@ant-design/web3';
+import { WalletColorful, MetaMaskColorful } from '@ant-design/web3-icons';
 export default function ConnectWallet() {
-	const { isConnected } = useAccount();
+	const { isConnected, address, connector } = useAccount();
 	const { connectors, connect } = useConnect();
-	if (isConnected) {
-		return <div></div>;
-	} else {
-		return (
-			<>
-				<Button
-					variant="solid"
-					color="orange"
-					onClick={() => {
-						console.log('connecting');
-						connect({ connector: connectors[0] });
-					}}
-				>
-					连接Metamask钱包
-				</Button>
-			</>
-		);
-	}
+	return (
+		<div>
+			<ConnectButton
+				icon={isConnected ? <WalletColorful /> : <MetaMaskColorful />}
+				account={isConnected ? { address: address! } : undefined}
+				onConnectClick={() => {
+					connect({ connector: connectors[0] });
+				}}
+				onDisconnectClick={() => {
+					connector?.disconnect();
+				}}
+			/>
+		</div>
+	);
 }
