a10b2b2094fa7d7d02c899569474be12dd7cfd52
joe <musketeerdt@gmail.com>
2025-05-22 07:13:45 +0800

    extract cards grid

--- app/dashboard/tracing/(overview)/tracing-cards/index.tsx
+++ app/dashboard/tracing/(overview)/tracing-cards/index.tsx
@@ -5,10 +5,8 @@ import {
 	fetchUserByEmail,
 	fetchUserProductTypes,
 } from '@/app/lib/data';
+import CardsGrid from '@/app/ui/components/cardsGrid';
 import { auth } from '@/auth';
-import { Card, Empty, Spin } from 'antd';
-import Meta from 'antd/es/card/Meta';
-import Link from 'next/link';
 export default async function TracingCards() {
 	const session = await auth();
 	if (!session?.user?.email) {
@@ -19,45 +17,16 @@ export default async function TracingCards() {
 	const product_types = await fetchUserProductTypes(user!);
 	const onChainNumber = await fetchOnChainNumber(product_types);
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-			{product_types.map((pt) => {
-				return (
-					<Link
-						key={pt.id}
-						href={`/dashboard/tracing/product_type/${pt.id}`}
-					>
-						<Card
-							hoverable
-							style={{ width: 240 }}
-							cover={
-								<div className="h-44 pt-1">
-									<img
-										className="max-h-44 block mx-auto"
-										alt={pt.name}
-										src={`/api/pinita/file?cid=${pt.coverCid}`}
-									/>
-								</div>
-							}
-						>
-							<Meta
-								title={
-									<p className="text-center truncate">
-										{pt.name}
-									</p>
-								}
-								description={
-									<div className="text-center">
-										<span>
-											上链数量:
-											{onChainNumber.get(pt.id)}
-										</span>
-									</div>
-								}
-							></Meta>
-						</Card>
-					</Link>
-				);
+		<CardsGrid
+			cardsContent={product_types.map((pt) => {
+				return {
+					id: pt.id,
+					coverUrl: `/api/pinita/file?cid=${pt.coverCid}`,
+					linkHref: `/dashboard/tracing/product_type/${pt.id}`,
+					title: pt.name,
+					description: `上链数量:${onChainNumber.get(pt.id)}`,
+				};
 			})}
-		</div>
+		/>
 	);
 }
